6ceef9c35fab00dd4f4116f14a9031579295a464
elanajaja <elanjaja01@gmail.com>
2024-03-13 15:05:40 -0700

    1-create_user.sql file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,15 +1,8 @@
--- Sscript that creates the table force_name on your MySQL server.
+-- creates the MySQL server user user_0d_1
 
--- Create database
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 
--- Create user and set password
-CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Give user_0d_2 SELECT privilege in the database hbtn_0d_2
-GRANT 
-SELECT 
-  ON `hbtn_0d_2`.* TO 'user_0d_2' @'localhost';
-
--- For changes to take effect immediatelY
-FLUSH PRIVILEGES;
+GRANT ALL
+ON *.*
+TO 'user_0d_1'@'localhost';
